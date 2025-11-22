4f63c9e885044b5e8a6f758a4bea87825046e2d1
John Kattner <you@example.com>
2025-11-21 19:45:08 -0600

    doing stuff

--- src/main/java/net/eggsaregood/duckymod/block/ModBlocks.java
+++ src/main/java/net/eggsaregood/duckymod/block/ModBlocks.java
@@ -11,17 +11,23 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-
 import java.util.function.Supplier;
+
+import static net.eggsaregood.duckymod.sound.ModSounds.DUCKY_BLOCK_BREAK;
+import static net.minecraftforge.common.data.SoundDefinition.Sound.sound;
 
 public class ModBlocks {
 
     public static DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, DuckyMod.MOD_ID);
 
-    public static RegistryObject<Block> DUCKY_BLOCK = registerBlock("ducky_block",
-            () -> new Block(BlockBehaviour.Properties.of()));
-                    
+
+    public static final RegistryObject<Block> DUCKY_BLOCK = registerBlock("ducky_block",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .sound(SoundType.ANVIL)));
+
+
+
 
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
@@ -37,4 +43,6 @@ public class ModBlocks {
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
     }
-}
+
+
+    }
